031b10df393dc97cd0ae2ebfe2fc91650175efc1
Ivan Gamanovich <therealigmiller@gmail.com>
2023-10-03 23:28:21 +0300

    faet: add new updater

--- XtraWork/Program.cs
+++ XtraWork/Program.cs
@@ -1,25 +1,54 @@
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using XtraWork.Repositories;
+using XtraWork.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("XtraWork");
+
+builder.Services.AddDbContext<XtraWorkContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<TitleRepository>();
+builder.Services.AddScoped<EmployeeRepository>();
+
+builder.Services.AddScoped<TitleService>();
+builder.Services.AddScoped<EmployeeService>();
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<XtraWorkContext>();
+context.Database.EnsureCreated();
+
+app.UseExceptionHandler(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception != null)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
 
+            var response = new
+            {
+                message = exception.GetBaseException().Message
+            };
+
+            await context.Response.WriteAsJsonAsync(response);
+        }
+    });
+});
+
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
